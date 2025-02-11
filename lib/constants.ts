036184fcb5ed6f3af696bfc17c5fa65948cad519
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-11 21:27:13 +0300

    commit

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Azo Gebeya";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A Next.js-based e-commerce platform tailored for the Ethiopian market, providing a seamless and secure online shopping experience.";
